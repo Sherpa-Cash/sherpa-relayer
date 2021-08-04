55f497ed2d866123c686540ac1cd11eeac95f077
Tyler Smith <mail@tcry.pt>
2021-08-03 23:22:02 -0700

    TWEAK: Add mainnet contracts to config.

--- src/token-config.js
+++ src/token-config.js
@@ -1,5 +1,16 @@
 module.exports = {
   instances: {
+    netId43114: {
+      avax: {
+        instanceAddress: {
+          10: '0xe1376DeF383D1656f5a40B6ba31F8C035BFc26Aa',
+          100: '0x7CE57f6a5a135eb1a8e9640Af1eff9665ade00D9',
+          500: '0x6ceB170e3ec0fAfaE3Be5A02FEFb81F524FE85C5',
+        },
+        symbol: 'AVAX',
+        decimals: 18
+      }
+    },
     netId43113: {
       avax: {
         instanceAddress: {
