cbdbee07a22dcdc0ad0eeaa8b4d60356cdb036b7
rabib <53358432+rabib@users.noreply.github.com>
2021-08-11 19:40:39 -0400

    adding ETHSherpa addresses mainnet

--- src/token-config.js
+++ src/token-config.js
@@ -3,13 +3,25 @@ module.exports = {
     netId43113: {
       avax: {
         instanceAddress: {
-          1: '0xda1A79024d0D5ad60C40dCC31DBb433388e26584',
-          10: '0x6F101749A8260580eD8Ae5513d8D75bcEd59Ec40',
-          100: '0xc2B5a1786bdbe38664e1a6bff5126F92dC1078A4',
+          10: '0x66F4f64f9Dce3eB1476af5E1f530228b8eD0a63f',
+          100: '0x9B247a159a0bB483098620656ADf8Bcac1365A1f',
+          500: '0x345aeAbf09Bc4Ee603af672d2De925483c35A5b0',
+        },
+        symbol: 'AVAX',
+        decimals: 18
+      }
+    },
+    netId43114: {
+      avax: {
+        instanceAddress: {
+          10: '0xe1376DeF383D1656f5a40B6ba31F8C035BFc26Aa',
+          100: '0x7CE57f6a5a135eb1a8e9640Af1eff9665ade00D9',
+          500: '0x6ceB170e3ec0fAfaE3Be5A02FEFb81F524FE85C5',
         },
         symbol: 'AVAX',
         decimals: 18
       }
     }
-  }
+  },
+  
 }
